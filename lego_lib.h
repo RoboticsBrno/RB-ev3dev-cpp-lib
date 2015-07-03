2d930bcae06dfbd14dbe5f14da9294470feba0d9
Jan Mr??zek <email@honzamrazek.cz>
2015-07-03 22:13:51 +0200

    Support for motor added

--- lego_lib.h
+++ lego_lib.h
@@ -128,3 +128,133 @@ extern StopButton g_stop_button;
  * Initializes stop button
  */
 void use_stop_button(port_type button_port);
+
+/**
+ * Wrapper of the motor class
+ */
+class Motor {
+public:
+	Motor(port_type motor_pin) : motor(motor_pin) {	}
+
+	/**
+	 * Direct access to underlying ev3dev class
+	 */
+	ev3dev::motor& backdoor() {
+		return motor;
+	}
+
+	/**
+	 * Resets the motor to the default state
+	 */
+	void reset() {
+		motor.set_command(ev3dev::motor::command_reset);
+	}
+
+	/**
+	 * Returns number of encoder position per revolution
+	 */
+	int getCountPerRev() {
+		return motor.count_per_rot();
+	}
+
+	/**
+	 * Turns motor into speed mode and sets its speed
+	 * @argument speed speed in range <-2000;2000>
+	 * 			 speed is in encoder positions per second
+	 */
+	void setSpeed(int speed) {
+		if(speed < 2000 || speed > 2000)
+			throw std::runtime_error("Max speed exceeded; "
+					+ std::to_string(abs(speed)) + "of 2000");
+		motor.set_speed_regulation_enabled("on");
+		motor.set_speed_sp(speed);
+		motor.run_forever();
+	}
+
+	/**
+	 * Returns the current speed of the motor
+	 * Speed is in encoder positions per second
+	 */
+	int getSpeed() {
+		return motor.speed_sp();
+	}
+
+	/**
+	 * Setups the PID constants for speed regulation
+	 * ToDo: Find out the range for these constants!
+	 */
+	void setSpeedPID(int p, int i, int d) {
+		motor.set_speed_regulation_p(p);
+		motor.set_speed_regulation_i(i);
+		motor.set_speed_regulation_d(d);
+	}
+
+	/**
+	 * Setups start/stop ramp times
+	 * @param start time in ms to achieve full speed
+	 * @param stop time in ms to break down completely
+	 */
+	void setSpeedRamps(int start, int stop) {
+		motor.set_ramp_up_sp(start);
+		motor.set_ramp_down_sp(stop);
+	}
+
+	/**
+	 * Changes value of the encoder position to given value
+	 */
+	void setEncoderPosition(int position) {
+		motor.set_position(position);
+	}
+
+	/**
+	 * Turns the motor into position regulation and
+	 * moves to the given position
+	 */
+	void setPositionAbs(int position) {
+		motor.set_speed_regulation_enabled("off");
+		motor.set_position_sp(position);
+		motor.run_to_abs_pos();
+	}
+
+	/**
+	 * Turns the motor into position regulation and
+	 * moves to given relative position
+	 */
+	void setPositionRel(int position) {
+		motor.set_speed_regulation_enabled("off");
+		motor.set_position_sp(position);
+		motor.run_to_rel_pos();
+	}
+
+	/**
+	 * Setups the PID constants for position regulation
+	 * ToDo: Find out the range for these constants!
+	 */
+	void setPositionPID(int p, int i, int d) {
+		motor.set_position_p(p);
+		motor.set_position_i(i);
+		motor.set_position_d(d);
+	}
+
+	/**
+	 * Ignores all regulation and sets the motor power
+	 * directly
+	 * @param power power in percents <-100;100>
+	 */
+	void setPower(int power) {
+		if(power < -100 || power > 100)
+			throw std::runtime_error("Power out of range: "
+					+ std::to_string(abs(power)) + "/100");
+		motor.set_speed_regulation_enabled("off");
+		motor.set_duty_cycle_sp(power);
+	}
+
+	/**
+	 * Returns current power to motor in percents
+	 */
+	int getPower() {
+		return motor.duty_cycle();
+	}
+private:
+	ev3dev::motor motor;
+};
